057d8c8f5b9094ba455b1c0105870121239896b2
ApocalyVec <zli12@wpi.edu>
2019-04-18 15:14:55 -0400

    add classifier_operations.py

---
+++ classifier_operations.py
@@ -0,0 +1,14 @@
+from fastdtw import fastdtw
+from scipy.spatial.distance import euclidean
+
+
+def sim_between_seq(seq1, seq2):
+    """
+    calculate the similarity between sequence 1 and sequence 2 using DTW
+
+    TODO customizable distance type using Scipy
+    :param seq1:
+    :param seq2:
+    :return float: return the similarity between sequence 1 and sequence 2
+    """
+    return fastdtw(seq1, seq2, dist=euclidean)
