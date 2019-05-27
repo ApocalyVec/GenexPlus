003119b72c9bd4d29260325e8521bff1ca65f4b3
YuncM98 <lorraine-c98@outlook.com>
2019-05-27 15:20:29 -0400

    Updated brute_force module

--- brute_force.py
+++ brute_force.py
@@ -4,12 +4,9 @@ from dtaidistance import dtw  # source code https://github.com/wannesm/dtaidista
 
 from group_operations import generate_source
 
-features_to_append = [0, 1, 2, 3, 4]
-res_list, time_series_dict, global_min, global_max = generate_source('2013e_001_2_channels_02backs.csv', features_to_append)
 
-
-def get_distance(dwtfunc, *args):
-    distance, paths = dwtfunc(*args)
+def get_distance(func, *args):
+    distance, paths = func(*args)
     return distance
 
 def best_match_ts(query, ts_dict):
@@ -88,3 +85,9 @@ def slice_list(ts_list, length):
         sliced_list.append(ts_list[i:i+length])
 
     return sliced_list
+
+
+if __name__ == '__main__':
+    features_to_append = [0, 1, 2, 3, 4]
+    res_list, time_series_dict, global_min, global_max = generate_source('2013e_001_2_channels_02backs.csv',
+                                                                         features_to_append)
