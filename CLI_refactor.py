3ca90fa7f338bc36e25c1b6c56d1a686d6676dea
ApocalyVec <zli12@wpi.edu>
2019-06-04 22:57:55 -0400

    revert CLI_refactor

--- CLI_refactor.py
+++ CLI_refactor.py
@@ -42,11 +42,6 @@ import pickle
 
 # commands
 from pyspark import SparkContext, SparkConf
-
-from CLI import spark_context_not_set_error, gp_not_opened_error, load_file_not_found_error, get_arg_error, \
-    no_group_before_cluster_error
-from CLI_errors import no_cluster_before_query_error, no_query_result_before_plot
-from CLI_exceptions import DuplicateIDError
 from GenexPlusProject import GenexPlusProject
 from cluster_operations import cluster
 from data_operations import normalize_ts_with_min_max, get_data
@@ -75,6 +70,51 @@ style = Style.from_dict({
     # 'path':     'ansicyan underline',
 })
 
+def gp_not_opened_error():
+    err_msg = FormattedText([
+        ('class:error', 'No GenexPlus Project Opened, please use the open command to open a GenexPlus Project'),
+    ])
+    print_formatted_text(err_msg, style=style)
+
+
+def spark_context_not_set_error():
+    err_msg = FormattedText([
+        ('class:error', 'Spark Context not set, please use set command to set spark context'),
+    ])
+    print_formatted_text(err_msg, style=style)
+
+
+def no_group_before_cluster_error():
+    err_msg = FormattedText([
+        ('class:error', 'Please group the data before clustering'),
+    ])
+    print_formatted_text(err_msg, style=style)
+
+def no_cluster_before_query_error():
+    err_msg = FormattedText([
+        ('class:error', 'Please cluster the data before querying'),
+    ])
+    print_formatted_text(err_msg, style=style)
+
+def get_arg_error():
+    err_msg = FormattedText([
+        ('class:error', 'Please group the data before clustering'),
+    ])
+    print_formatted_text(err_msg, style=style)
+
+def load_file_not_found_error(missing_file):
+    err_msg = FormattedText([
+        ('class:error',
+         'File ' + missing_file + ' not found'),
+    ])
+    print_formatted_text(err_msg, style=style)
+
+def no_query_result_before_plot():
+    err_msg = FormattedText([
+        ('class:error',
+         'Please get query result before plot it'),
+    ])
+    print_formatted_text(err_msg, style=style)
 
 def create_new_project(SAVED_DATASET_DIR, args):
     isCreateNewProject = prompt("Project " + args[
